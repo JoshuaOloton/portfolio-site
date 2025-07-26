fe953dde1f0681357874c99a79a2837e4a78ad6c
Joshua Oloton <olotonjoshua@gmail.com>
2025-07-26 13:01:49 +0100

    feat: dynamic visibility of project carousel arrows

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { ArrowLeft, ArrowRight, ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, Dot, SquareArrowOutUpRight } from "lucide-react";
 import { inter } from "@/app/fonts";
 import { motion } from "motion/react";
 import { projects } from "@/app/data/projects";
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useScrollToSection } from "@/app/hooks/useScrollToSection";
 
 type Props = {
@@ -13,6 +13,30 @@ type Props = {
 
 const Projects = ({ ref }: Props) => {
   const projectsCarouselRef = useRef<HTMLDivElement | null>(null);
+
+  // State to manage visibility of left and right buttons
+  // Initially, show the right button and hide the left button
+  const [showLeftButton, setShowLeftButton] = useState(false);
+  const [showRightButton, setShowRightButton] = useState(true);
+
+  useEffect(() => {
+    const ref = projectsCarouselRef.current;
+    
+    if (ref) {
+      ref.scrollLeft = 0; // Reset scroll position on mount
+
+      const handleScroll = () => {
+        const { scrollLeft, scrollWidth, clientWidth } = ref;
+        setShowLeftButton(scrollLeft > 0);
+        setShowRightButton(scrollWidth - scrollLeft - clientWidth > 1);
+      };
+
+      ref.addEventListener("scroll", handleScroll);
+      return () => {
+        ref?.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
   
   return (
     <section className={`${inter.className} section_layout h-full`} ref={ref} id="projects">
@@ -23,55 +47,84 @@ const Projects = ({ ref }: Props) => {
             Explore the projects that showcase my skills and creative solutions.
           </p>
         </div>
-        <div className="flex gap-8 w-full overflow-x-scroll snap-x snap-mandatory no-scrollbar" ref={projectsCarouselRef}>
+        <div aria-label="Projects Carousel" className="flex gap-8 w-full overflow-x-scroll snap-x snap-mandatory no-scrollbar" ref={projectsCarouselRef}>
           {projects.map((project) => (
-            <div
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5, ease: "easeOut" }}
               key={project.id}
-              className="w-full px-52 py-10 flex-shrink-0 flex flex-col gap-0 snap-center"
+              className="w-full px-52 py-10 flex-shrink-0 flex flex-col gap-0 snap-center bg-[#1A1A1A] border border-gray-800 rounded-2xl shadow-lg"
             >
               <motion.img 
-                initial={{ y: -50 }}
+                initial={{ y: -100 }}
                 whileInView={{ y: 0 }}
-                // transition={{ delay: 0.1 }}
+                // transition={{ ease: "easeOut" }}
                 src={project.image} 
                 className="w-3/5 mx-auto my-3" 
                 alt={project.title} 
               />
-              <h4 className="text-2xl tracking-wider font-bold my-2">{project.title}</h4>
+              <h4 className="text-2xl tracking-wider font-bold my-2">
+                {project.title}{" "}
+                <a href={project.visit} target="_blank" rel="noopener noreferrer" className="text-yellow-500 hover:text-yellow-400 transition-colors duration-200">
+                  <SquareArrowOutUpRight className="inline" />
+                </a>
+              </h4>
               <hr className="w-1/5 m-auto border border-yellow-500 my-2" />
               <p className="text-justify tracking-wide leading-7 my-3">{project.description.substring(0,200)}...</p>
-              <div>
-                {project.tags.map((tag) => (
-                  <span key={tag}>{tag}</span>
+              <div className="flex flex-wrap items-center justify-center gap-1 my-3">
+                {project.tags.map((tag, index) => (
+                  <React.Fragment key={index}>
+                    <motion.span 
+                      initial={{ opacity: 0, y: 10 }} 
+                      whileInView={{ opacity: 1, y: 0 }} 
+                      transition={{ delay: (index + 1) * 0.95/5 }} 
+                      className="bg-[#1F1F1F] text-gray-200 border border-gray-700 rounded-full px-4 py-1 text-sm font-medium shadow-sm hover:bg-[#2A2A2A] transition-colors duration-200"
+                      >
+                        {tag}
+                      </motion.span>
+                    <motion.span 
+                      initial={{ opacity: 0, y: 10 }} 
+                      whileInView={{ opacity: 1, y: 0 }} 
+                      transition={{ delay: (index + 1) * 0.95/5 }} 
+                      className="text-gray-300 last:hidden"
+                    >
+                      <Dot size={26} />
+                    </motion.span>
+                  </React.Fragment>
                 ))}
               </div>
-              <a href={project.source}>Source Code</a>
-              <a href={project.visit}>Visit Site</a>
-            </div>
+            </motion.div>
           ))}
         </div>
-        <ChevronLeft
-            onClick={() => {
-              if (projectsCarouselRef.current) {
-                projectsCarouselRef.current.scrollBy({
-                  left: -300,
-                  behavior: "smooth",
-                });
-              }
-            }}
-            className="absolute left-4 top-1/2 -translate-y-1/2 cursor-pointer"
-          />
-          <ChevronRight
-            onClick={() => {
-              if (projectsCarouselRef.current) {
-                projectsCarouselRef.current.scrollBy({
+        { showLeftButton && (
+          <div className="absolute left-4 top-1/2 -translate-y-1/2 group p-1 rounded-full">
+            <ChevronLeft
+                onClick={() => {
+                  projectsCarouselRef.current?.scrollBy({
+                    left: -300,
+                    behavior: "smooth",
+                  });
+                }}
+                size={26}
+                className="cursor-pointer opacity-30 group-hover:opacity-100 group-hover:duration-500 transition-opacity"
+                />
+          </div>
+        )}
+        { showRightButton && (
+          <div className="absolute right-4 top-1/2 -translate-y-1/2 group p-1 rounded-full">
+            <ChevronRight
+              onClick={() => {
+                projectsCarouselRef.current?.scrollBy({
                   left: 300,
                   behavior: "smooth",
                 });
-              }
-            }}
-            className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
-          />
+              }}
+              size={26}
+              className="cursor-pointer opacity-30 group-hover:opacity-100 group-hover:duration-500 transition-opacity"
+            />
+          </div>
+        )}   
       </div>
     </section>
   );
