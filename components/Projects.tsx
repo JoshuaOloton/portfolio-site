a175f2308e28d684dad7638d1d7be45a57daa851
Joshua Oloton <olotonjoshua@gmail.com>
2025-08-12 14:56:42 +0100

    chore: small cleanup

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -46,20 +46,22 @@ const Projects = ({ ref }: Props) => {
 
   return (
     <section
+      aria-labelledby="projects-heading"
       className={`${inter.className} section-layout h-full`}
       ref={ref}
       id="projects"
     >
       <div className="flex justify-center items-center relative h-screen">
         <div className="absolute top-4 md:top-6">
-          <motion.h3 
+          <motion.h3
+            id="projects-heading"
             className="section-header"
             initial="initial"
             whileInView="animate"
             transition={{
               staggerChildren: 0.02,
             }}
-            viewport={{ once: true }}
+            // viewport={{ once: true }}
           >
             {titleText.split("").map((char, index) => (
               <motion.span
@@ -80,7 +82,7 @@ const Projects = ({ ref }: Props) => {
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.5 }}
-            viewport={{ once: true }}
+            // viewport={{ once: true }}
           >
             Explore the projects that showcase my skills and creative solutions.
           </motion.p>
